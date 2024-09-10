c0ea3519c273b184113b1b71517872c7567be28a
Sebastian Tobito <sebastiantobito@gmail.com>
2024-09-10 14:07:48 -0500

    aggregando las variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("bienvenido a la rama feature-setup");
+        String cuentaNumero = "123456789";
+        double balance = 1500.75;
+        int contrasena = 1234;
+
+        int[] montosTransacciones ={200, 100, -100, -50};
+
+        balance += montosTransacciones[0];
+        if(balance > 0 && contrasena == 1234){
+            System.out.println("Acceso concedido");
+        }
+
+        balance++;
+        String estatus = (balance <0)?"Deuda":"Credito";
+        System.out.println("Estado de cuenta: "+estatus);
+        System.out.println(balance);
     }
 }
